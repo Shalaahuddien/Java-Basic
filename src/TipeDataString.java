b90162caec11d623fd4fbea7fb585717a2368032
Shalaahuddien Daffa <77540526+Shalaahuddien@users.noreply.github.com>
2023-02-23 00:58:22 +0700

    Menggabungkan String

--- src/TipeDataString.java
+++ src/TipeDataString.java
@@ -3,8 +3,12 @@ public class TipeDataString {
         String firstName = "Eko kurniawan";
         String lastName = "Khannedy";
 
+        String fullName = firstName +""+ lastName;
+
         System.out.println(firstName);
         System.out.println(lastName);
+
+        System.out.println(fullName);
 
     }
 }
