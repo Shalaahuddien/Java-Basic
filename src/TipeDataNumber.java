092b433ce0b94bae8b84a1ffb58759ae9b136783
Shalaahuddien Daffa <77540526+Shalaahuddien@users.noreply.github.com>
2023-02-22 14:01:13 +0700

    TipeDataNumber&float

--- src/TipeDataNumber.java
+++ src/TipeDataNumber.java
@@ -5,5 +5,9 @@ public class TipeDataNumber {
         int iniInt = 10000000;
         long iniLong = 1000000000;
         long iniLong2 = 1000000000L;
+
+        float iniFloat = 10.10F;
+        double iniDouble = 10.10;
+
     }
 }
