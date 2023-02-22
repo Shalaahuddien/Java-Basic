1f87c38f00aa16eeeabbaf3196677c017122f3cd
Shalaahuddien Daffa <77540526+Shalaahuddien@users.noreply.github.com>
2023-02-22 13:29:40 +0700

    TipeDataNumber

---
+++ src/TipeDataNumber.java
@@ -0,0 +1,9 @@
+public class TipeDataNumber {
+    public static void main(String[] args) {
+        byte iniByte = 100;
+        short iniShort = 1000;
+        int iniInt = 10000000;
+        long iniLong = 1000000000;
+        long iniLong2 = 1000000000L;
+    }
+}
