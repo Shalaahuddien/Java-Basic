ff8a28802022d6f0b6c7228d92a4ab56c8b6b5cd
Shalaahuddien Daffa <77540526+Shalaahuddien@users.noreply.github.com>
2023-02-22 14:20:29 +0700

    Konversi-Numbers

---
+++ src/KonversiNumber.java
@@ -0,0 +1,11 @@
+public class KonversiNumber {
+    public static void main(String[] args) {
+        byte iniByte = 10;
+        short iniShort = iniByte;
+        int iniInt = iniShort;
+
+//        int iniInt2 = 1000;
+        byte iniByte2 = (byte) iniInt;
+
+    }
+}
