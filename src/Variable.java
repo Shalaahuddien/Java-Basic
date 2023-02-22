d766806fe025aab023c7afdbec711c30f7dd24d6
Shalaahuddien Daffa <77540526+Shalaahuddien@users.noreply.github.com>
2023-02-23 01:07:12 +0700

    Kode_variable

---
+++ src/Variable.java
@@ -0,0 +1,14 @@
+public class Variable {
+    public static void main(String[] args) {
+        String name;
+        name = "Eko Kurniawan Khanedy";
+
+        int age = 30;
+        String address = "Indonesia";
+
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(address);
+
+    }
+}
