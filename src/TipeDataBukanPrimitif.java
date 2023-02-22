559a56ec17b3b4b7375ba8cec15f6a9883c2b58a
Shalaahuddien Daffa <77540526+Shalaahuddien@users.noreply.github.com>
2023-02-23 01:33:21 +0700

    TipeDataBukanPrimitif

---
+++ src/TipeDataBukanPrimitif.java
@@ -0,0 +1,19 @@
+public class TipeDataBukanPrimitif {
+    public static void main(String[] args) {
+        Integer iniInteger = 100;
+        Long iniLong = 10000L;
+
+        Byte iniByte = null;
+
+        System.out.println(iniByte);
+
+        iniByte = 100;
+
+        System.out.println(iniByte);
+
+        int iniInt = 100;
+
+        Integer iniInteger2 = iniInt;
+
+    }
+}
