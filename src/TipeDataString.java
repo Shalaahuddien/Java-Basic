e462a555e7f4be72f3986a60ff6b2e49213e0bbe
Shalaahuddien Daffa <77540526+Shalaahuddien@users.noreply.github.com>
2023-02-23 00:53:43 +0700

    Tipe-Data-String

---
+++ src/TipeDataString.java
@@ -0,0 +1,10 @@
+public class TipeDataString {
+    public static void main(String[] args) {
+        String firstName = "Eko kurniawan";
+        String lastName = "Khannedy";
+
+        System.out.println(firstName);
+        System.out.println(lastName);
+
+    }
+}
