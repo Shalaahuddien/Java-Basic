baecfd99dda4a17c72dba52ae8d81276787b261f
Shalaahuddien Daffa <77540526+Shalaahuddien@users.noreply.github.com>
2023-02-23 09:36:37 +0700

    OperasiMatematika

---
+++ src/OperasiMatematika.java
@@ -0,0 +1,12 @@
+public class OperasiMatematika {
+    public static void main(String[] args) {
+        int a = 100;
+        int b = 10;
+
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+        System.out.println(a%b);
+    }
+}
