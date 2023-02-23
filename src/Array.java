cbbb2737682ad383783539a21663b9250fe45574
Shalaahuddien Daffa <77540526+Shalaahuddien@users.noreply.github.com>
2023-02-23 09:13:30 +0700

    Array

---
+++ src/Array.java
@@ -0,0 +1,20 @@
+public class Array {
+    public static void main(String[] args) {
+        String[] stringArray;
+        stringArray = new String[3];
+
+        stringArray[0] = "Eko";
+        stringArray[1] = "Kurniawan";
+        stringArray[2] = "Khanedy";
+
+        System.out.println(stringArray[0]);
+        System.out.println(stringArray[1]);
+        System.out.println(stringArray[2]);
+
+        stringArray[0] = "Budi";
+
+        System.out.println(stringArray[0]);
+
+        String[] stringArray2 = new String[3];
+    }
+}
