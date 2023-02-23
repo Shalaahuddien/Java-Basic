7a0a077a6dc16ce81387afdb01ccd8e2b5806b6b
Shalaahuddien Daffa <77540526+Shalaahuddien@users.noreply.github.com>
2023-02-23 11:12:37 +0700

    SwitchDenganYield

--- src/SwitchStatement.java
+++ src/SwitchStatement.java
@@ -43,5 +43,19 @@ public class SwitchStatement {
 
         System.out.println(ucapan);
 
+         ucapan = switch (nilai)
+        {
+            case "A":
+                yield "Wow Anda Lulus Dengan Baik";
+            case "B", "C":
+                yield "Anda Lulus";
+            case "D":
+                yield "Anda Tidak Lulus";
+            default:
+                yield "Mungkin Anda Salah Jurusan";
+        };
+
+        System.out.println(ucapan);
+
     }
 }
