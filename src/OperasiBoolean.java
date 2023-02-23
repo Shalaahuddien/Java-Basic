817e98fb543b2cf7c3fe66affa9a2056de9e1c4c
Shalaahuddien Daffa <77540526+Shalaahuddien@users.noreply.github.com>
2023-02-23 09:59:47 +0700

    OperasiBoolean

---
+++ src/OperasiBoolean.java
@@ -0,0 +1,12 @@
+public class OperasiBoolean {
+    public static void main(String[] args) {
+        var absen = 70;
+        var nilaiAkhir = 80;
+
+        var lulusAbsen = absen >= 75;
+        var lulusNilaiAkhir = nilaiAkhir >= 75;
+
+        var lulus = lulusAbsen && lulusNilaiAkhir;
+        System.out.println(lulus);
+    }
+}
