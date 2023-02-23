d7d4613deab3bfdf68a168a7942c8e45ab9f0cbc
Shalaahuddien Daffa <77540526+Shalaahuddien@users.noreply.github.com>
2023-02-23 10:10:45 +0700

    Statement

--- src/ExpressionStatementDanBlock.java
+++ src/ExpressionStatementDanBlock.java
@@ -1,8 +1,22 @@
+import java.util.Date;
+
 public class ExpressionStatementDanBlock {
     public static void main(String[] args) {
         int value;
         value = 10;
 
         System.out.println(value = 100);
+
+        //assigment statement
+        double aValue = 8933.234;
+
+        //increament statement
+        aValue++;
+
+        //method invocation statement
+        System.out.println("Hello World");
+
+        //object creation statement
+        Date date = new Date();
     }
 }
