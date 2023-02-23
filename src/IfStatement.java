59995a731fe13eca6bb99d9fabbc88b39d488b50
Shalaahuddien Daffa <77540526+Shalaahuddien@users.noreply.github.com>
2023-02-23 10:25:45 +0700

    IfStatement

---
+++ src/IfStatement.java
@@ -0,0 +1,16 @@
+public class IfStatement {
+    public static void main(String[] args) {
+        var nilai = 80;
+        var absen = 90;
+
+        var lulusNilai = nilai >= 75;
+        var lulusAbsen = absen >= 75;
+
+        var lulus = lulusNilai && lulusAbsen;
+
+        if(nilai >= 75 && absen >= 75)
+        {
+            System.out.println("Anda Lulus");
+        }
+    }
+}
