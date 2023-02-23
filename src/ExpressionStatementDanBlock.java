a3ed278d46b33a76d981a520dc005ee9915264cf
Shalaahuddien Daffa <77540526+Shalaahuddien@users.noreply.github.com>
2023-02-23 10:05:23 +0700

    Expression

---
+++ src/ExpressionStatementDanBlock.java
@@ -0,0 +1,8 @@
+public class ExpressionStatementDanBlock {
+    public static void main(String[] args) {
+        int value;
+        value = 10;
+
+        System.out.println(value = 100);
+    }
+}
