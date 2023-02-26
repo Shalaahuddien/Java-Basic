135bfc79fcf483e18c9e72a322072750ea8d7e2f
Shalaahuddien Daffa <77540526+Shalaahuddien@users.noreply.github.com>
2023-02-26 10:44:40 +0700

    ForLoop

---
+++ src/ForLoop.java
@@ -0,0 +1,20 @@
+public class ForLoop {
+    public static void main(String[] args) {
+
+//        var counter = 1;
+//
+//        for (; counter <= 10;)
+//        {
+//            System.out.println("Perulangan ke-"+counter);
+//            counter++;
+//        }
+
+//        System.out.println(counter);
+
+//        for (;;)
+//        {
+//            System.out.println("Perulangan Tanpa Henti");
+//        }
+
+    }
+}
