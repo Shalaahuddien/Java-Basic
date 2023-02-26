92bffc96980d7dc723937c3dc72b36b0fca30a2d
Shalaahuddien Daffa <77540526+Shalaahuddien@users.noreply.github.com>
2023-02-26 11:52:50 +0700

    Method_Parameter

--- src/MethodParameter.java
+++ src/MethodParameter.java
@@ -1,6 +1,8 @@
 public class MethodParameter {
     public static void main(String[] args) {
         sayHello("Eko", " Kurniawan");
+        sayHello("Budi", " Nugraha");
+        sayHello("Joko", " Nugroho");
     }
     static void sayHello(String firstName, String lastName)
     {
