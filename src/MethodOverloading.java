df3d260b5f0d380c79a31f596b07fe6f1743521b
Shalaahuddien Daffa <77540526+Shalaahuddien@users.noreply.github.com>
2023-02-26 12:50:15 +0700

    Method_Overloading

--- src/MethodOverloading.java
+++ src/MethodOverloading.java
@@ -6,6 +6,12 @@ public class MethodOverloading {
     {
         System.out.println("Hello");
     }
+
+//    static void sayHello(String name)
+//    {
+//        System.out.println("Hello " + name);
+//    }
+
     static void sayHello(String firstName)
     {
         System.out.println("Hello " + firstName);
