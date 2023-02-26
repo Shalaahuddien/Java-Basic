f25d647b46696f0c19c64b1f9fe7ca57ff26e820
Shalaahuddien Daffa <77540526+Shalaahuddien@users.noreply.github.com>
2023-02-26 11:58:55 +0700

    Method_Return-Value

--- src/MethodReturnValue.java
+++ src/MethodReturnValue.java
@@ -1,5 +1,16 @@
 public class MethodReturnValue {
     public static void main(String[] args) {
+        var a = 100;
+        var b = 200;
+        var c = sum(a, b);
 
+        System.out.println(c);
     }
+
+    static int sum(int value1, int value2)
+    {
+        var total = value1 + value2;
+        return total;
+    }
+
 }
