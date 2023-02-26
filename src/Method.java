4095dbbf7f00b25603817b8cc7abad9473b37c2d
Shalaahuddien Daffa <77540526+Shalaahuddien@users.noreply.github.com>
2023-02-26 11:29:46 +0700

    Method

---
+++ src/Method.java
@@ -0,0 +1,11 @@
+public class Method {
+    public static void main(String[] args) {
+        sayHelloWord();
+    }
+
+    static void  sayHelloWord()
+    {
+        System.out.println("Hello World");
+    }
+
+}
