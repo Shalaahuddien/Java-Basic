2ff07718b3e72581ed427ce0cba2b67a478ae4af
Shalaahuddien Daffa <77540526+Shalaahuddien@users.noreply.github.com>
2023-02-26 13:18:34 +0700

    Scope

--- src/Scope.java
+++ src/Scope.java
@@ -2,4 +2,19 @@ public class Scope {
     public static void main(String[] args) {
 
     }
+
+    static void sayHello(String name)
+    {
+        String hello = "Hello " + name;
+        if (!name.isBlank())
+        {
+            String hi = "Hi " + name;
+            System.out.println(hi);
+        }
+
+        System.out.println(hello);
+//        System.out.println(hi); //error
+
+    }
+
 }
