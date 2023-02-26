1891a845601f9b75758c342c04f9a97b96ebbbb7
Shalaahuddien Daffa <77540526+Shalaahuddien@users.noreply.github.com>
2023-02-26 13:10:02 +0700

    RecursiveMethode

--- src/RecursiveMethod.java
+++ src/RecursiveMethod.java
@@ -29,4 +29,17 @@ public class RecursiveMethod {
         }
     }
 
+    static void loop(int value)
+    {
+        if(value == 0)
+        {
+            System.out.println("Selesai");
+        }
+        else
+        {
+            System.out.println("Loop-" + value);
+            loop(value -1);
+        }
+    }
+
 }
