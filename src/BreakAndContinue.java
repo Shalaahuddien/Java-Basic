fdcb6350f99fdef6fd10add3b73a7325a5d6153a
Shalaahuddien Daffa <77540526+Shalaahuddien@users.noreply.github.com>
2023-02-26 11:05:24 +0700

    Break

---
+++ src/BreakAndContinue.java
@@ -0,0 +1,15 @@
+public class BreakAndContinue {
+    public static void main(String[] args) {
+        var counter = 1;
+        while(true)
+        {
+            System.out.println("Perulangan ke-" + counter);
+            counter++;
+
+            if(counter > 10)
+            {
+                break;
+            }
+        }
+    }
+}
