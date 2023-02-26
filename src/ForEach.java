ecce3bc4560ddbd9e222b8ec2565d3ab9a2efbd7
Shalaahuddien Daffa <77540526+Shalaahuddien@users.noreply.github.com>
2023-02-26 11:20:46 +0700

    Tanpa_For_Each

---
+++ src/ForEach.java
@@ -0,0 +1,14 @@
+public class ForEach {
+    public static void main(String[] args) {
+        String[] array =
+                {
+                        "Eko","Kurniawan","Khanedy",
+                        "Programmer","Zaman","Now"
+                };
+
+        for (int i = 0; i < array.length; i++)
+        {
+            System.out.println(array[i]);
+        }
+    }
+}
