51d8d0a461e2b133a3f9c562a41dc2789bb44828
Shalaahuddien Daffa <77540526+Shalaahuddien@users.noreply.github.com>
2023-02-26 12:32:37 +0700

    Method_variable_Argument

--- src/MethodVariableArgument.java
+++ src/MethodVariableArgument.java
@@ -1,15 +1,18 @@
 public class MethodVariableArgument {
     public static void main(String[] args) {
 
+        int[] values = {80,80,80,80,80};
+        sayCongrats("EKO", values);
+
     }
 
     static void sayCongrats(String name, int[] values){
-        int total = 0;
+        var total = 0;
         for(var value : values)
         {
             total += value;
         }
-        int finalValue = total / values.length;
+        var finalValue = total / values.length;
 
         if(finalValue >= 75)
         {
