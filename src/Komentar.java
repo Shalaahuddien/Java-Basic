6081dee3fba02490c955e3d7201305e2082cb194
Shalaahuddien Daffa <77540526+Shalaahuddien@users.noreply.github.com>
2023-02-26 13:32:24 +0700

    Komentar

---
+++ src/Komentar.java
@@ -0,0 +1,20 @@
+public class Komentar {
+    public static void main(String[] args) {
+
+    }
+
+    /**
+     * Menghitung Jumlah a+b
+     *
+     * @param a nilai a
+     * @param a nilai b
+     * @return a + b
+     */
+
+    static int sum(int a, int b)
+    {
+        //Jumlahkan a + b
+        return a + b;
+    }
+
+}
