26947b73f515ec15b2767b30a4f6058433fdb8fa
Shalaahuddien Daffa <77540526+Shalaahuddien@users.noreply.github.com>
2023-02-26 12:37:29 +0700

    Method_variable_Argument

--- src/MethodVariableArgument.java
+++ src/MethodVariableArgument.java
@@ -6,17 +6,35 @@ public class MethodVariableArgument {
 
     }
 
-    static void sayCongrats(String name, int[] values){
-        var total = 0;
+//    static void sayCongrats(String name, int[] values){
+//        var total = 0;
+//        for(var value : values)
+//        {
+//            total += value;
+//        }
+//        var finalValue = total / values.length;
+//
+//        if(finalValue >= 75)
+//        {
+//            System.out.println("Selamat " + name + ", Anda Lulus");
+//        }
+//        else
+//        {
+//            System.out.println("Maaf " + name + ", Anda Lulus");
+//        }
+//    }
+
+    static void sayCongrats(String name, int... values)
+    {
+        int total = 0;
         for(var value : values)
         {
             total += value;
         }
-        var finalValue = total / values.length;
-
+        int finalValue = total / values.length;
         if(finalValue >= 75)
         {
-            System.out.println("Selamat " + name + ", Anda Lulus");
+            System.out.println("Selamat " +name+", Anda Lulus");
         }
         else
         {
