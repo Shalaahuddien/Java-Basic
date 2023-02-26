124922149a7cf9f798f64ee191b998325aa41f26
Shalaahuddien Daffa <77540526+Shalaahuddien@users.noreply.github.com>
2023-02-26 12:59:17 +0700

    RecursiveMethode

--- src/RecursiveMethod.java
+++ src/RecursiveMethod.java
@@ -1,9 +1,11 @@
 public class RecursiveMethod {
     public static void main(String[] args) {
 
+        System.out.println(factorialLoop(5));
+
     }
 
-    static int factorial(int value)
+    static int factorialLoop(int value)
     {
         var result = 1;
         for(int i = 1; i <= value; i++)
