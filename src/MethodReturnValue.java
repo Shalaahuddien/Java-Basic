b0d203f3c24bcaebab346f318365cb30b4937454
Shalaahuddien Daffa <77540526+Shalaahuddien@users.noreply.github.com>
2023-02-26 12:14:36 +0700

    Method_Return-Value

--- src/MethodReturnValue.java
+++ src/MethodReturnValue.java
@@ -19,4 +19,17 @@ public class MethodReturnValue {
         return total;
     }
 
+    static int hitung(int value1, String operasi, int value2)
+    {
+        switch (operasi)
+        {
+            case "+" :
+                return value1 + value2;
+            case "-" :
+                return value1 + value2;
+            default:
+                return 0;
+        }
+    }
+
 }
